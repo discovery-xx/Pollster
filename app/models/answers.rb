4ac7e11692a3e9cdbc3342dc05f875ce4442f985
swapnil <swapnil@joshsoftware.com>
2011-12-09 20:22:36 +0530

    Added validations

--- app/models/answers.rb
+++ app/models/answers.rb
@@ -1,6 +1,6 @@
 class Answers < ActiveRecord::Base
    belongs_to :poll    
    belongs_to :user
-   validates :option4, :presence => true
-   validates :answer6, :presence => true
+   validates :flag, :presence => true
+   validates :fuzz, :presence => true
 end
